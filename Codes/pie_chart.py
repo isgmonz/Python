fe2feb7c6175666bbb43de4c20bf34855886bc53
Ismael <isgmonz@gmail.com>
2023-07-23 01:30:54 -0600

    C-matplotlib

---
+++ Codes/pie_chart.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
+sizes = [15, 30, 45, 10]
+
+fig, ax = plt.subplots()
+ax.pie(sizes, labels=labels)
+
+
+explode = (0, 0.1, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+
+fig, ax = plt.subplots()
+ax.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+       shadow=True, startangle=90)
+plt.show()
